6b7d3c89f9ca753289eecc1a616cdae8e03993d3
bonoo1 <bonoo74@gmail.com>
2022-02-22 19:33:45 +0900

    시간추가, 등록시 홈페이지로 이동 추가

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 from flask import redirect
+from datetime import datetime
 
 client = MongoClient('mongodb+srv://test:sparta@cluster0.7tst7.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
@@ -10,46 +11,68 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('main.html')
+    return render_template('index.html')
 
 
-@app.route('/create')
+@app.route('/new-review')
 def create():
-    return render_template('create.html')
+    return render_template('new_review.html')
 
 
-@app.route("/reviews", methods=["POST"])
+@app.route("/new-reviews", methods=["POST"])
 def reviews_create():
+
     name_receive = request.form['name_give']
     comment_receive = request.form['comment_give']
     vaccine_receive = request.form['vaccine_give']
     number_receive = request.form['number_give']
+    created_at = datetime.now()
 
     doc = {
         'name': name_receive,
         'comment': comment_receive,
         'vaccine': vaccine_receive,
-        'number': number_receive
+        'number': number_receive,
+        'created_at': created_at
+
     }
     db.vaccines.insert_one(doc)
 
-    return jsonify({'msg': '저장 완료!'})
+    return jsonify({'msg': '작성완료'})
 
 
-@app.route("/reviews", methods=["GET"])
-def reviews():
-    vaccine_list = list(db.vaccines.find({}, {'_id': False}))
-    return jsonify({'list': vaccine_list})
+@app.route('/api/reviews', methods=['GET'])
+def index():
+    vaccine = request.args.get('vaccine', "")
+    degree = request.args.get('degree', "")
+    page = int(request.args.get('page', 1))
+
+    options = {}
+
+    if len(vaccine) > 0 and len(degree) > 0:
+        options['vaccine'] = vaccine
+        options['degree'] = degree
+    elif len(vaccine) > 0:
+        options['vaccine'] = vaccine
+    elif len(degree) > 0:
+        options['degree'] = degree
+
+    review_list = list(db.vaccines.find(options).limit(9).skip((page - 1) * 9))
+
+    results = []
+
+    for document in review_list:
+        document['_id'] = str(document['_id'])
+        results.append(document)
+
+    return jsonify({'reviews': results})
+
 
 @app.route("/reviews")
 def board_create():
-    return render_template('create.html')
-
-@app.route("/")
-def board_cancle():
-    return render_template('main.html')
-
+    return render_template('new_review.html')
 
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
+
